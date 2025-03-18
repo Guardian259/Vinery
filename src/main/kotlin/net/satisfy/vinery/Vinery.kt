440a92e23628dc5265fa8f36eba0a8b076ec36ad
Guardian <primaryapplogin@gmail.com>
2025-03-18 10:29:40 -0500

    Migrated Config to Simple Config Setup

--- src/main/kotlin/net/satisfy/vinery/Vinery.kt
+++ src/main/kotlin/net/satisfy/vinery/Vinery.kt
@@ -33,8 +33,50 @@ class Vinery : ModInitializer {
             private set
     }
 
-    //TODO: Convert the Existing Cloth Config System to Simple Config
     private fun getDefaultConfig(filename: String): String {
-        return """"""
+        return """
+            #============================================================
+            #=====================|Block Settings|=======================
+            #============================================================
+            #totalFermentationTime (min = 1, max = 10000)
+            totalFermentationTime=6000
+            #maxFluidLevel (min = 1, max = 10000)
+            maxFluidLevel=100
+            #maxFluidIncrease (min = 1, max = 10000)
+            maxFluidIncrease=25
+            #applePressMashingTime (min = 1, max = 10000)
+            applePressMashingTime=600
+            #applePressFermentationTime (min = 1, max = 10000)
+            applePressFermentationTime=800
+            #cherryGrowthChance (min = 0, max = 1)
+            cherryGrowthChance=0.4
+            #appleGrowthChance (min = 0, max = 1)
+            appleGrowthChance=0.4
+            #grapeGrowthChance (min = 0, max = 1)
+            grapeGrowthChance=0.5
+            #============================================================
+            #=====================|Wine Settings|========================
+            #============================================================
+            #startDuration (min = 1, max = 100000)
+            startDuration=1800
+            #maxDuration (min = 1, max = 100000)
+            maxDuration=15000
+            #maxLevel (min = 1, max = 10)
+            maxLevel=5
+            #durationPerYear (min = 1, max = 10000)
+            durationPerYear=200
+            #daysPerYear (min = 1, max = 100)
+            daysPerYear=24
+            #yearsPerEffectLevel (min = 1, max = 100)
+            yearsPerEffectLevel=6
+            #============================================================
+            #====================|Trader Settings|=======================
+            #============================================================
+            #spawnChance (min = 0, max = 1)
+            spawnChance=0.5
+            spawnWithMules=true
+            #spawnDelay (min = 0, max = 72000)
+            spawnDelay=48000
+        """.trimIndent()
     }
 }
